3a50a32b37f34ad618f9fcfb9dc54d1bbc302f42
LasToPe <lassep95@gmail.com>
2018-07-12 16:21:06 +0200

    Light setup of Navbar

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -4,32 +4,18 @@ import { Link, NavLink } from 'react-router-dom';
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
         return <div className='main-nav'>
-                <div className='navbar navbar-inverse'>
+                <div className='navbar navbar-expand-sm'>
                 <div className='navbar-header'>
                     <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
+                        <span className='navbar-toggle-icon'>&#9776;</span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>LassePedersen_website</Link>
+                    <Link className='navbar-brand' to={ '/' }>Lasse Pedersen</Link>
                 </div>
-                <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
                     <ul className='nav navbar-nav'>
-                        <li>
+                        <li className='nav-item'>
                             <NavLink to={ '/' } exact activeClassName='active'>
                                 <span className='glyphicon glyphicon-home'></span> Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
                             </NavLink>
                         </li>
                     </ul>
